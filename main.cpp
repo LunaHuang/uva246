ca900957c4583bf6766bb9d686b4fd31ef565b8b
Luna Huang <lunahuang@weintek.com>
2022-06-27 18:33:29 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,245 +1,177 @@
 #include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <queue>
 #include <set>
 #include <sstream>
-#include <regex>
-#include <cctype>
-#include <map>
 #include <vector>
-#include <algorithm>
 
 #if defined(WITH_GTEST)
 #include <gtest/gtest.h>
 #endif
 
-enum Level {
-    Even,
-    Light,
-    Heavy,
-};
+#define PokerMax 52
+// last number is deck
+#define TotalPilesNum 8
+#define DeckNum 7
 
-class Counterfeit
-{
-    // constructions
+class Poker {
+  // constructions
 public:
-    explicit Counterfeit(std::istream &is);
-
-    // methods
+  // methods
 public:
-    void show();
-    std::string check(void);
-    void parser_coin(std::string &input);
+  void init_piles(std::string &input);
+  std::string game_play(void);
+  void show();
 
-    // data
 private:
-    std::map< std::string, int > even_coin_;
-    std::map< std::string, int > light_coin_;
-    std::map< std::string, int > heavy_coin_;
-    std::vector< std::string > light_coin_group_;
-    std::vector< std::string > heavy_coin_group_;
+  void pick_up_check(int order);
+  bool combination_check(int a, int b, int c);
+
+  // data
+private:
+  std::array<std::deque<int>, TotalPilesNum> piles_;
+  std::set<std::vector<std::deque<int>>> pile_set_;
+  int play_count;
+  std::string game_result;
 };
 
-Counterfeit::Counterfeit(std::istream &is)
-{
-    even_coin_.clear();
-    light_coin_.clear();
-    heavy_coin_.clear();
-    light_coin_group_.clear();
-    heavy_coin_group_.clear();
-    for (int i = 0; i < 3; i++) {
-        std::string input;
-        std::getline(is, input);
-        //		std::cout << "input: " << input << std::endl;
-        parser_coin(input);
-    }
+bool Poker::combination_check(int a, int b, int c) {
+  int sum = a + b + c;
+  return (sum % 10 == 0);
 }
 
-void Counterfeit::parser_coin(std::string &input)
-{
-    std::stringstream ss(input);
-    std::string token1;
-    std::string token2;
-    std::string element;
-    std::getline(ss, token1, ' ');
-    std::getline(ss, token2, ' ');
-    std::getline(ss, element, ' ');
-    if (element.compare("even") == 0) {
-        for (int i = 0; i < token1.length(); i++) {
-            std::string ch = token1.substr(i, 1);
-            even_coin_[ch] = Level::Even;
-        }
-        for (int i = 0; i < token2.length(); i++) {
-            std::string ch = token2.substr(i, 1);
-            even_coin_[ch] = Level::Even;
-        }
-    } else if (element.compare("up") == 0) {
-        heavy_coin_group_.push_back(token1);
-        for (int i = 0; i < token1.length(); i++) {
-            std::string ch = token1.substr(i, 1);
-            heavy_coin_[ch] = Level::Heavy;
-        }
-        light_coin_group_.push_back(token2);
-        for (int i = 0; i < token2.length(); i++) {
-            std::string ch = token2.substr(i, 1);
-            light_coin_[ch] = Level::Light;
-        }
-    } else if (element.compare("down") == 0) {
-        light_coin_group_.push_back(token1);
-        for (int i = 0; i < token1.length(); i++) {
-            std::string ch = token1.substr(i, 1);
-            light_coin_[ch] = Level::Light;
-        }
-        heavy_coin_group_.push_back(token2);
-        for (int i = 0; i < token2.length(); i++) {
-            std::string ch = token2.substr(i, 1);
-            heavy_coin_[ch] = Level::Heavy;
-        }
+void Poker::pick_up_check(int order) {
+  while (1) {
+
+    if (piles_[order].size() < 3)
+      break;
+
+    int size = piles_[order].size();
+    int f = piles_[order].front();
+    int s = piles_[order][1];
+    int e3 = piles_[order][size - 3];
+    int e2 = piles_[order][size - 2];
+    int last = piles_[order].back();
+    if (combination_check(f, s, last) == true) {
+      piles_[DeckNum].push_back(f);
+      piles_[DeckNum].push_back(s);
+      piles_[DeckNum].push_back(last);
+      piles_[order].pop_front();
+      piles_[order].pop_front();
+      piles_[order].pop_back();
+      continue;
     }
+    if (combination_check(f, e2, last) == true) {
+      piles_[DeckNum].push_back(f);
+      piles_[DeckNum].push_back(e2);
+      piles_[DeckNum].push_back(last);
+      piles_[order].pop_front();
+      piles_[order].pop_back();
+      piles_[order].pop_back();
+      continue;
+    }
+    if (combination_check(e3, e2, last) == true) {
+      piles_[DeckNum].push_back(e3);
+      piles_[DeckNum].push_back(e2);
+      piles_[DeckNum].push_back(last);
+      piles_[order].pop_back();
+      piles_[order].pop_back();
+      piles_[order].pop_back();
+      continue;
+    }
+    break;
+  }
 }
 
-void Counterfeit::show(void)
-{
-    std::cout << "even_coin_ : " << std::endl;
-    for (auto &it : even_coin_)
-        std::cout << it.first << " ";
+void Poker::init_piles(std::string &input) {
+  // clear private data
+  play_count = 0;
+  pile_set_.clear();
+  game_result.clear();
+  for (int i = 0; i < TotalPilesNum; i++)
+    piles_[i].clear();
+
+  // init private data
+  std::stringstream ss(input);
+  std::string token;
+  while (std::getline(ss, token, ' ')) {
+    int num = stoi(token);
+    if (play_count < TotalPilesNum) {
+      piles_[play_count].push_back(num);
+      play_count++;
+    } else {
+      piles_[DeckNum].push_back(num);
+    }
+  }
+}
+
+std::string Poker::game_play() {
+  int order = 0;
+  for (int count = play_count;; count++) {
+
+    int poker_num = piles_[DeckNum].front();
+    piles_[DeckNum].pop_front();
+    piles_[order].push_back(poker_num);
+
+    pick_up_check(order);
+
+    std::vector<std::deque<int>> piles_vector(piles_.begin(), piles_.end());
+    if (pile_set_.find(piles_vector) != pile_set_.end()) {
+      game_result = std::string("Draw: ") + std::to_string(count);
+      break;
+    }
+    pile_set_.insert(piles_vector);
+
+    if (piles_[DeckNum].size() == 0) {
+      game_result = std::string("Loss: ") + std::to_string(count);
+      break;
+    }
+
+    if (piles_[DeckNum].size() == PokerMax) {
+      game_result = std::string("Win : ") + std::to_string(count);
+      break;
+    }
+
+    order = (order + 1) % (TotalPilesNum - 1);
+    while (piles_[order].size() == 0) {
+      order = (order + 1) % (TotalPilesNum - 1);
+    }
+  }
+  return game_result;
+}
+
+void Poker::show(void) {
+  std::cout << "deck : " << std::endl;
+  for (auto it : piles_[DeckNum])
+    std::cout << it << " ";
+  std::cout << std::endl;
+
+  for (int i = 0; i < (TotalPilesNum - 1); i++) {
+    std::cout << "piles_" << i << ":" << std::endl;
+    for (auto it : piles_[i])
+      std::cout << it << " ";
     std::cout << std::endl;
-    std::cout << "up_coin_ : " << std::endl;
-    for (auto &it : light_coin_group_)
-        std::cout << it << " ";
-    std::cout << std::endl;
-    std::cout << "down_coin_ : " << std::endl;
-    for (auto &it : heavy_coin_group_)
-        std::cout << it << " ";
-    std::cout << std::endl;
+  }
 }
 
-std::string Counterfeit::check(void)
-{
-    std::string out(" is the counterfeit coin and it is ");
-
-    // 1. remove even coin
-    for (int i = 0; i < light_coin_group_.size(); i++) {
-        for (auto &it : even_coin_) {
-            std::size_t found = light_coin_group_[i].find(it.first);
-            if (found != std::string::npos)
-                light_coin_group_[i].erase(found, 1);
-        }
-        if (light_coin_group_[i].length() == 0)
-            light_coin_group_.erase(light_coin_group_.begin() + i);
-    }
-    for (int i = 0; i < heavy_coin_group_.size(); i++) {
-        for (auto &it : even_coin_) {
-            std::size_t found = heavy_coin_group_[i].find(it.first);
-            if (found != std::string::npos)
-                heavy_coin_group_[i].erase(found, 1);
-        }
-        if (heavy_coin_group_[i].length() == 0)
-            heavy_coin_group_.erase(heavy_coin_group_.begin() + i);
-    }
-    if ((light_coin_group_.size() == 1) && (heavy_coin_group_.size() == 0)) {
-        out = light_coin_group_[0] + out + "light.";
-        return out;
-    }
-    if ((light_coin_group_.size() == 0) && (heavy_coin_group_.size() == 1)) {
-        out = heavy_coin_group_[0] + out + "heavy.";
-        return out;
-    }
-
-    //2. remove coin on light/heavy group at same time
-    for (int i = 0; i < light_coin_group_.size(); i++) {
-        for (auto &it : heavy_coin_) {
-            std::size_t found = light_coin_group_[i].find(it.first);
-            if (found != std::string::npos)
-                light_coin_group_[i].erase(found, 1);
-        }
-    }
-    for (int i = 0; i < heavy_coin_group_.size(); i++) {
-        for (auto &it : light_coin_) {
-            std::size_t found = heavy_coin_group_[i].find(it.first);
-            if (found != std::string::npos)
-                heavy_coin_group_[i].erase(found, 1);
-        }
-    }
-    std::map< std::string, int > coin_map;
-    for (int i = 0; i < light_coin_group_.size(); i++) {
-        if (light_coin_group_[i].length() == 1) {
-            if (heavy_coin_group_[i].length() == 0) {
-                out = light_coin_group_[i] + out + "light.";
-                return out;
-            }
-            if (coin_map.count(light_coin_group_[i])) {
-                out = light_coin_group_[i] + out + "light.";
-                return out;
-            }
-            coin_map[light_coin_group_[i]] = 1;
-            continue;
-        }
-        for (char c : light_coin_group_[i]) {
-            std::string tmp_string;
-            tmp_string.push_back(c);
-            if (coin_map.count(tmp_string)) {
-                coin_map[tmp_string]++;
-            }
-        }
-    }
-    for (auto &it : coin_map) {
-        if (it.second == 3) {
-            out = it.first + out + "light.";
-            return out;
-        }
-    }
-    coin_map.clear();
-    for (int i = 0; i < heavy_coin_group_.size(); i++) {
-        if (heavy_coin_group_[i].length() == 1) {
-            if (light_coin_group_[i].length() == 0) {
-                out = heavy_coin_group_[i] + out + "heavy.";
-                return out;
-            }
-            if (coin_map.count(heavy_coin_group_[i])) {
-                out = heavy_coin_group_[i] + out + "heavy.";
-                return out;
-            }
-            coin_map[heavy_coin_group_[i]] = 1;
-            continue;
-        }
-        for (char c : heavy_coin_group_[i]) {
-            std::string tmp_string;
-            tmp_string.push_back(c);
-            if (coin_map.count(tmp_string)) {
-                coin_map[tmp_string]++;
-            }
-        }
-    }
-    for (auto &it : coin_map) {
-        if (it.second == 3) {
-            out = it.first + out + "heavy.";
-            return out;
-        }
-    }
-	out.clear();
-    return out;
-}
-
-void solve_uva_problem(std::istream &is, std::ostream &os)
-{
+void solve_uva_problem(std::istream &is, std::ostream &os) {
+  Poker poker;
+  while (1) {
     std::string input;
     std::getline(is, input);
-    int counterfeit_num = stoi(input);
-    while (counterfeit_num > 0) {
-        Counterfeit counterfeit(is);
-        os << counterfeit.check() << std::endl;
-        //	counterfeit.show();
-        counterfeit_num = counterfeit_num - 1;
-    }
-    std::cout << std::endl;
+    if (input.compare("0") == 0)
+      break;
+    poker.init_piles(input);
+    os << poker.game_play() << std::endl;
+  }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 #if !defined(WITH_GTEST)
-    solve_uva_problem(std::cin, std::cout);
+  solve_uva_problem(std::cin, std::cout);
 #else
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 #endif
 }
